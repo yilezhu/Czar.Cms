2f4136ff93a22df66dc9a43721bcf268af3093a1
zhulei <1015657114@qq.com>
2018-12-12 11:57:45 +0800

    编写根据数据库生成实体的代码

---
+++ src/Czar.Cms.Core/CodeGenerator/CodeGenerator.cs
@@ -0,0 +1,174 @@
+﻿using Czar.Cms.Core.Models;
+using Czar.Cms.Core.Options;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Czar.Cms.Core.CodeGenerator
+{
+    /// <summary>
+    /// yilezhu
+    /// 2018.12.12
+    /// 代码生成器。参考自：Zxw.Framework.NetCore
+    /// <remarks>
+    /// 根据数据库表以及表对应的列生成对应的数据库实体
+    /// </remarks>
+    /// </summary>
+    public class CodeGenerator
+    {
+        private readonly string Delimiter = "\\";//分隔符，默认为windows下的\\分隔符
+
+        private static CodeGenerateOption _options;
+        public CodeGenerator(IOptions<CodeGenerateOption> options)
+        {
+            if (options == null) {
+                throw new ArgumentNullException(nameof(options));
+            }
+            else
+            {
+                _options = options.Value;
+            }
+            var path = AppDomain.CurrentDomain.BaseDirectory;
+            var flag = path.IndexOf("/bin");
+            if (flag > 0)
+                Delimiter = "/";//如果可以取到值，修改分割符
+        }
+
+        /// <summary>
+        /// 根据数据库连接字符串生成数据库表对应的Model层代码
+        /// </summary>
+        /// <param name="isCoveredExsited">是否覆盖已存在的同名文件</param>
+        public void GenerateAllCodesFromDatabase(bool isCoveredExsited = true)
+        {
+            //TODO 从数据库获取表列表以及生成实体对象
+        }
+
+
+        private void GenerateEntity(DbTable table, bool ifExsitedCovered = false)
+        {
+            var modelPath = _options.OutputPath + Delimiter + "Models";
+            if (!Directory.Exists(modelPath))
+            {
+                Directory.CreateDirectory(modelPath);
+            }
+
+            var fullPath = modelPath + Delimiter + table.TableName + ".cs";
+            if (File.Exists(fullPath) && !ifExsitedCovered)
+                return;
+
+            var pkTypeName = table.Columns.First(m => m.IsPrimaryKey).CSharpType;
+            var sb = new StringBuilder();
+            foreach (var column in table.Columns)
+            {
+                var tmp = GenerateEntityProperty(table.TableName, column);
+                sb.AppendLine(tmp);
+                sb.AppendLine();
+            }
+            var content = ReadTemplate("ModelTemplate.txt");
+            content = content.Replace("{ModelsNamespace}", _options.ModelsNamespace)
+                .Replace("{Comment}", table.TableComment)
+                .Replace("{ModelName}", table.TableName)
+                .Replace("{KeyTypeName}", pkTypeName)
+                .Replace("{ModelProperties}", sb.ToString());
+            WriteAndSave(fullPath, content);
+        }
+
+        private static string GenerateEntityProperty(string tableName, DbTableColumn column)
+        {
+            var sb = new StringBuilder();
+            if (!string.IsNullOrEmpty(column.Comment))
+            {
+                sb.AppendLine("\t\t/// <summary>");
+                sb.AppendLine("\t\t/// " + column.Comment);
+                sb.AppendLine("\t\t/// </summary>");
+            }
+            if (column.IsPrimaryKey)
+            {
+                sb.AppendLine("\t\t[Key]");
+                sb.AppendLine($"\t\t[Column(\"{tableName}Id\")]");
+                if (column.IsIdentity)
+                {
+                    sb.AppendLine("\t\t[DatabaseGenerated(DatabaseGeneratedOption.Identity)]");
+                }
+                sb.AppendLine($"\t\tpublic override {column.CSharpType} Id " + "{get;set;}");
+            }
+            else
+            {
+                if (!column.IsNullable)
+                {
+                    sb.AppendLine("\t\t[Required]");
+                }
+
+                if (column.ColumnLength.HasValue && column.ColumnLength.Value > 0)
+                {
+                    sb.AppendLine($"\t\t[MaxLength({column.ColumnLength.Value})]");
+                }
+                if (column.IsIdentity)
+                {
+                    sb.AppendLine("\t\t[DatabaseGenerated(DatabaseGeneratedOption.Identity)]");
+                }
+
+                var colType = column.CSharpType;
+                if (colType.ToLower() != "string" && colType.ToLower() != "byte[]" && colType.ToLower() != "object" &&
+                    column.IsNullable)
+                {
+                    colType = colType + "?";
+                }
+
+                sb.AppendLine($"\t\tpublic {colType} {column.ColName} " + "{get;set;}");
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// 从代码模板中读取内容
+        /// </summary>
+        /// <param name="templateName">模板名称，应包括文件扩展名称。比如：template.txt</param>
+        /// <returns></returns>
+        private string ReadTemplate(string templateName)
+        {
+            var currentAssembly = Assembly.GetExecutingAssembly();
+            var content = string.Empty;
+            using (var stream = currentAssembly.GetManifestResourceStream($"{currentAssembly.GetName().Name}.CodeTemplate.{templateName}"))
+            {
+                if (stream != null)
+                {
+                    using (var reader = new StreamReader(stream))
+                    {
+                        content = reader.ReadToEnd();
+                    }
+                }
+            }
+            return content;
+        }
+
+        /// <summary>
+        /// 写文件
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="content"></param>
+        private static void WriteAndSave(string fileName, string content)
+        {
+            //实例化一个文件流--->与写入文件相关联
+            using (var fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+            {
+                //实例化一个StreamWriter-->与fs相关联
+                using (var sw = new StreamWriter(fs))
+                {
+                    //开始写入
+                    sw.Write(content);
+                    //清空缓冲区
+                    sw.Flush();
+                    //关闭流
+                    sw.Close();
+                    fs.Close();
+                }
+            }
+        }
+    }
+}
---
+++ src/Czar.Cms.Core/Options/CodeGenerateOption.cs
@@ -0,0 +1,43 @@
+﻿using Czar.Cms.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Czar.Cms.Core.Options
+{
+    /// <summary>
+    /// yilezhu
+    /// 2018.12.12
+    /// 代码生成选项
+    /// </summary>
+    public class CodeGenerateOption
+    {
+        /// <summary>
+        /// 数据库连接字符串
+        /// </summary>
+        public string ConnectionString { get; set; }
+        /// <summary>
+        /// 数据库连接字符串
+        /// </summary>
+        public DatabaseType DbType  { get; set; }
+        /// <summary>
+        /// 作者
+        /// </summary>
+        public string Author { get; set; }
+
+        /// <summary>
+        /// 代码生成时间
+        /// </summary>
+        public string GeneratorTime { get; set; } = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+
+        /// <summary>
+        /// 输出路径
+        /// </summary>
+        public string OutputPath { get; set; }
+
+        /// <summary>
+        /// 实体命名空间
+        /// </summary>
+        public string ModelsNamespace { get; set; }
+    }
+}
