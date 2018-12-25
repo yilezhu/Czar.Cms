d375ef6f373b6a14db44dfa81388bb88001a6e21
zhulei <1015657114@qq.com>
2018-12-25 11:08:17 +0800

    测试代码整理

--- src/Czar.Cms.Test/BaseRepositoryTest.cs
+++ src/Czar.Cms.Test/BaseRepositoryTest.cs
@@ -33,9 +33,9 @@ namespace Czar.Cms.Test
         [Fact]
         public void TestBaseFactory()
         {
-            IServiceProvider serviceProvider = BuildServiceForSqlServer();
+            IServiceProvider serviceProvider = Common.BuildServiceForSqlServer();
             IArticleCategoryRepository categoryRepository = serviceProvider.GetService<IArticleCategoryRepository>();
-            var category = new ArticleCategory
+            var category1 = new ArticleCategory
             {
                 Title = "随笔1",
                 ParentId = 0,
@@ -48,21 +48,34 @@ namespace Czar.Cms.Test
                 SeoDescription = "随笔的SeoDescription",
                 IsDeleted = false,
             };
-            var categoryId = categoryRepository.Insert(category);
+            var category2 = new ArticleCategory
+            {
+                Title = null,
+                ParentId = 0,
+                ClassList = "",
+                ClassLayer = 0,
+                Sort = 0,
+                ImageUrl = "",
+                SeoTitle = "随笔的SEOTitle",
+                SeoKeywords = "随笔的SeoKeywords",
+                SeoDescription = "随笔的SeoDescription",
+                IsDeleted = false,
+            };
+            var categoryId = categoryRepository.Insert(category1);
+            var categoryId2 = categoryRepository.Insert(category2);
             var list = categoryRepository.GetList();
-            Assert.True(1 == list.Count());
-            Assert.Equal("随笔", list.FirstOrDefault().Title);
-            Assert.Equal("SQLServer", DatabaseType.SqlServer.ToString(), ignoreCase: true);
-            categoryRepository.Delete(categoryId.Value);
+            Assert.True(2 == list.Count());
+            Assert.Equal("随笔1", list.FirstOrDefault().Title);
+            categoryRepository.DeleteList("where 1=1");
             var count = categoryRepository.RecordCount();
             Assert.True(0 == count);
         }
 
 
         [Fact]
-        public void TestUnitOfWork()
+        public void TestUnitOfWorkNoException()
         {
-            IServiceProvider serviceProvider = BuildServiceForSqlServer();
+            IServiceProvider serviceProvider = Common.BuildServiceForSqlServer();
             var category1 = new ArticleCategory
             {
                 Title = "UnitOfWork分类1",
@@ -130,38 +143,77 @@ namespace Czar.Cms.Test
             #endregion
 
         }
-        /// <summary>
-        /// 构造依赖注入容器，然后传入参数
-        /// </summary>
-        /// <returns></returns>
-        public IServiceProvider BuildServiceForSqlServer()
+
+        [Fact]
+        public void TestUnitOfWorkWithException()
         {
-            var services = new ServiceCollection();
-            services.Configure<CodeGenerateOption>(options =>
+            IServiceProvider serviceProvider = Common.BuildServiceForSqlServer();
+            var category1 = new ArticleCategory
             {
-                options.ConnectionString = "Data Source=.;Initial Catalog=CzarCms;User ID=sa;Password=1;Persist Security Info=True;Max Pool Size=50;Min Pool Size=0;Connection Lifetime=300;";
-                options.DbType = DatabaseType.SqlServer.ToString();//数据库类型是SqlServer,其他数据类型参照枚举DatabaseType
-                options.Author = "yilezhu";//作者名称
-                options.OutputPath = "C:\\CzarCmsCodeGenerator";//模板代码生成的路径
-                options.ModelsNamespace = "Czar.Cms.Models";//实体命名空间
-                options.IRepositoryNamespace = "Czar.Cms.IRepository";//仓储接口命名空间
-                options.RepositoryNamespace = "Czar.Cms.Repository.SqlServer";//仓储命名空间
+                Title = "UnitOfWork分类1",
+                ParentId = 0,
+                ClassList = "",
+                ClassLayer = 0,
+                Sort = 0,
+                ImageUrl = "",
+                SeoTitle = "随笔的SEOTitle",
+                SeoKeywords = "随笔的SeoKeywords",
+                SeoDescription = "随笔的SeoDescription",
+                IsDeleted = false,
+            };
+            var category2 = new ArticleCategory
+            {
+                Title = null,
+                ParentId = 0,
+                ClassList = "",
+                ClassLayer = 0,
+                Sort = 0,
+                ImageUrl = "",
+                SeoTitle = "随笔的SEOTitle",
+                SeoKeywords = "随笔的SeoKeywords",
+                SeoDescription = "随笔的SeoDescription",
+                IsDeleted = false,
+            };
+            var menu = new Menu
+            {
+                ParentId = 0,
+                Name = "测试1",
+                AddManagerId = 1,
+                IsDelete = false,
+                IsSystem = false,
+                IsDisplay = true,
+                AddTime = DateTime.Now
 
-            });
-            services.Configure<DbOpion>("CzarCms", GetConfiguration().GetSection("DbOpion"));
-            services.AddScoped<IArticleRepository, ArticleRepository>();
-            services.AddScoped<IArticleCategoryRepository, ArticleCategoryRepository>();
-            services.AddScoped<IUnitOfWork, UnitOfWork>();
-            return services.BuildServiceProvider(); //构建服务提供程序
+
+            };
+            IArticleCategoryRepository categoryRepository = serviceProvider.GetService<IArticleCategoryRepository>();
+
+            var unitwork = serviceProvider.GetService<IUnitOfWork>();
+            //var categoryId = categoryRepository.Insert(category1);
+            var count = 0;
+            try
+            {
+                unitwork.Add(category1);
+                unitwork.Add(category2);//标题为null会抛出异常
+                unitwork.Add(menu);
+                count = unitwork.Commit();
+            }
+            catch (Exception ex)
+            {
+                
+            }
+
+            Assert.True(0 == count);
+            #region categoryRepository
+            var list = categoryRepository.GetList();
+            Assert.True(0 == list.Count());
+            var count2 = categoryRepository.DeleteList("where 1=1");
+            Assert.True(0 == count2);
+            var list2 = categoryRepository.GetList();
+            Assert.True(0 == list2.Count());
+            #endregion
+
         }
 
-        public IConfiguration GetConfiguration()
-        {
-            var builder = new ConfigurationBuilder()
-               .SetBasePath(AppContext.BaseDirectory)
-               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-               .AddEnvironmentVariables();
-            return builder.Build();
-        }
     }
 }
---
+++ src/Czar.Cms.Test/Common.cs
@@ -0,0 +1,62 @@
+﻿/**
+*┌──────────────────────────────────────────────────────────────┐
+*│　描    述：                                                    
+*│　作    者：yilezhu                                             
+*│　版    本：1.0                                                 
+*│　创建时间：2018/12/25 10:14:14                             
+*└──────────────────────────────────────────────────────────────┘
+*┌──────────────────────────────────────────────────────────────┐
+*│　命名空间： Czar.Cms.Test                                   
+*│　类    名： Common                                      
+*└──────────────────────────────────────────────────────────────┘
+*/
+using Czar.Cms.Core.Models;
+using Czar.Cms.Core.Options;
+using Czar.Cms.Core.Repository;
+using Czar.Cms.IRepository;
+using Czar.Cms.Repository.SqlServer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Czar.Cms.Test
+{
+    public class Common
+    {
+        /// <summary>
+        /// 构造依赖注入容器，然后传入参数
+        /// </summary>
+        /// <returns></returns>
+        public static IServiceProvider BuildServiceForSqlServer()
+        {
+            var services = new ServiceCollection();
+            services.Configure<CodeGenerateOption>(options =>
+            {
+                options.ConnectionString = "Data Source=.;Initial Catalog=CzarCms;User ID=sa;Password=1;Persist Security Info=True;Max Pool Size=50;Min Pool Size=0;Connection Lifetime=300;";
+                options.DbType = DatabaseType.SqlServer.ToString();//数据库类型是SqlServer,其他数据类型参照枚举DatabaseType
+                options.Author = "yilezhu";//作者名称
+                options.OutputPath = "C:\\CzarCmsCodeGenerator";//模板代码生成的路径
+                options.ModelsNamespace = "Czar.Cms.Models";//实体命名空间
+                options.IRepositoryNamespace = "Czar.Cms.IRepository";//仓储接口命名空间
+                options.RepositoryNamespace = "Czar.Cms.Repository.SqlServer";//仓储命名空间
+
+            });
+            services.Configure<DbOpion>("CzarCms", GetConfiguration().GetSection("DbOpion"));
+            services.AddScoped<IArticleRepository, ArticleRepository>();
+            services.AddScoped<IArticleCategoryRepository, ArticleCategoryRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            return services.BuildServiceProvider(); //构建服务提供程序
+        }
+
+        public static IConfiguration GetConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+               .SetBasePath(AppContext.BaseDirectory)
+               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+               .AddEnvironmentVariables();
+            return builder.Build();
+        }
+    }
+}
