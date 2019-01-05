0af3ccbd45041685253a50c52282536a74506a5d
zhulei <1015657114@qq.com>
2019-01-05 18:00:51 +0800

    泛型帮助类、

---
+++ src/Czar.Cms.Core/Helper/GenericHelpers.cs
@@ -0,0 +1,26 @@
+﻿/**
+*┌──────────────────────────────────────────────────────────────┐
+*│　描    述：List转成Tree                                                    
+*│　作    者：yilezhu                                             
+*│　版    本：1.0                                                 
+*│　创建时间：2019/1/5 12:14:39                             
+*└──────────────────────────────────────────────────────────────┘
+*┌──────────────────────────────────────────────────────────────┐
+*│　命名空间： Czar.Cms.Core.Helper                                   
+*│　类    名： GenericHelpers                                      
+*└──────────────────────────────────────────────────────────────┘
+*/
+using Czar.Cms.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Czar.Cms.Core.Helper
+{
+    public static class GenericHelpers
+    {
+    
+        
+    }
+}
