d12b18671f0a0078a4a7d04976fc036eaee2b50c
zhulei <1015657114@qq.com>
2019-03-13 10:44:55 +0800

    初始化Quartz的类库并写调度服务类

---
+++ src/Czar.Cms.ViewModels/ResultModel/ScheduleResult.cs
@@ -0,0 +1,22 @@
+﻿/**
+*┌──────────────────────────────────────────────────────────────┐
+*│　描    述：任务调度结果                                                    
+*│　作    者：yilezhu                                             
+*│　版    本：1.0                                                 
+*│　创建时间：2019/3/13 10:22:01                             
+*└──────────────────────────────────────────────────────────────┘
+*┌──────────────────────────────────────────────────────────────┐
+*│　命名空间： Czar.Cms.ViewModels.ResultModel                                   
+*│　类    名： ScheduleResult                                      
+*└──────────────────────────────────────────────────────────────┘
+*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Czar.Cms.ViewModels
+{
+    public class ScheduleResult:BaseResult
+    {
+    }
+}
